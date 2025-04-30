8189414f01de0fda660b2b69d1c995334e8efbae
Bhrugen Patel <bhrugen1990@gmail.com>
2025-04-30 13:24:15 -0500

    Section 2 - Container Service Interface

---
+++ AzureBlobProject/Services/ContainerService.cs
@@ -0,0 +1,26 @@
+﻿
+namespace AzureBlobProject.Services
+{
+    public class ContainerService : IContainerService
+    {
+        public Task CreateContainer(string containerName)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task DeleteContainer(string containerName)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<List<string>> GetAllContainer()
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<List<string>> GetAllContainerAndBlobs()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
