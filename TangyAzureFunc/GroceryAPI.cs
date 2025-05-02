8b726c7aac7ebfd4a929f67005563cd3f0468a9f
Bhrugen Patel <bhrugen1990@gmail.com>
2025-05-02 12:26:00 -0500

    Section 7 - All Endpoints

--- TangyAzureFunc/GroceryAPI.cs
+++ TangyAzureFunc/GroceryAPI.cs
@@ -28,6 +28,15 @@ namespace TangyAzureFunc
             return new OkObjectResult(_dbContext.GroceryItems.ToList());
         }
 
+        [Function("GetGroceryById")]
+        public IActionResult GetGroceryById([HttpTrigger(AuthorizationLevel.Function, "get", Route = "GroceryList/{id}")] HttpRequest req,string id)
+        {
+            _logger.LogInformation("Getting Grocery List Item - " + id);
+
+
+            return new OkObjectResult(_dbContext.GroceryItems.FirstOrDefault(u=>u.Id==id));
+        }
+
         [Function("CreateGrocery")]
         public async Task<IActionResult> CreateGrocery([HttpTrigger(AuthorizationLevel.Function, "post", Route = "GroceryList")] HttpRequest req)
         {
@@ -42,7 +51,44 @@ namespace TangyAzureFunc
             _dbContext.GroceryItems.Add(groceryItem);
             _dbContext.SaveChanges();
 
-            return new OkObjectResult(_dbContext.GroceryItems.ToList());
+            return new OkObjectResult(groceryItem);
         }
+
+        [Function("UpdateGrocery")]
+        public async Task<IActionResult> UpdateGrocery([HttpTrigger(AuthorizationLevel.Function, "post", Route = "GroceryList/{id}")] HttpRequest req,string id)
+        {
+            _logger.LogInformation("Updating Grocery List Item.");
+
+            var item = _dbContext.GroceryItems.FirstOrDefault(u => u.Id == id);
+            if (item == null)
+            {
+                return new NotFoundObjectResult("Item not found.");
+            }
+            string requestBody = await new StreamReader(req.Body).ReadToEndAsync();
+            GroceryItem_Upsert? data = JsonConvert.DeserializeObject<GroceryItem_Upsert>(requestBody);
+            if (!string.IsNullOrEmpty(data.Name))
+            {
+                item.Name = data.Name;
+                _dbContext.SaveChanges();
+            }
+            return new OkObjectResult(item);
+        }
+
+        [Function("DeleteGrocery")]
+        public async Task<IActionResult> DeleteGrocery([HttpTrigger(AuthorizationLevel.Function, "post", Route = "GroceryList/{id}")] HttpRequest req, string id)
+        {
+            _logger.LogInformation("Delete Grocery List Item.");
+
+            var item = _dbContext.GroceryItems.FirstOrDefault(u => u.Id == id);
+            if (item == null)
+            {
+                return new NotFoundObjectResult("Item not found.");
+            }
+           _dbContext.GroceryItems.Remove(item);
+            _dbContext.SaveChanges();
+            return new OkResult();
+        }
+
+
     }
 }
